37b6e0a3827b06eaf795b35169ba9bbadf6e62d5
tudosealexandru23 <tudosealexandru23@stud.ase.ro>
2025-12-11 19:24:43 +0200

    Add files via upload

---
+++ moto.cpp
@@ -0,0 +1,185 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Motocicleta {
+private:
+	const int ID;
+	static int nrMotociclete;
+	string marca;
+	int nrPiese;
+	float* pret;
+public:
+	Motocicleta() :ID(++nrMotociclete) {
+		this->marca = "Necunoscuta";
+		this->nrPiese = 0;
+		this->pret = nullptr;
+	}
+
+	Motocicleta(string _marca, int nrPiese, float* pret) :ID(++nrMotociclete) {
+		marca = _marca;
+		this->nrPiese = nrPiese;
+		//shallow copy
+		//this->pret = pret;
+
+		//deep copy
+		this->pret = new float[nrPiese];
+		for (int i = 0; i < nrPiese; i++) {
+			this->pret[i] = pret[i];
+		}
+		delete[] pret;
+	}
+
+	Motocicleta(const Motocicleta& m) :ID(++nrMotociclete) {
+		this->marca = m.marca;
+		this->nrPiese = m.nrPiese;
+		this->pret = new float[m.nrPiese];
+		for (int i = 0; i < m.nrPiese; i++) {
+			this->pret[i] = m.pret[i];
+		}
+	}
+
+	const Motocicleta& operator=(const Motocicleta& m) {
+		if (this == &m) {
+			return *this;
+		}
+		this->marca = m.marca;
+		this->nrPiese = m.nrPiese;
+		if (this->pret != nullptr) {
+			delete[]this->pret;
+		}
+		this->pret = new float[m.nrPiese];
+		for (int i = 0; i < m.nrPiese; i++) {
+			this->pret[i] = m.pret[i];
+		}
+		return *this;
+	}
+
+	
+	Motocicleta operator+=(float pret) {
+		this->nrPiese++;
+		float* aux = new float[this->nrPiese];
+		for (int i = 0; i < this->nrPiese - 1; i++) {
+			aux[i] = this->pret[i];
+		}
+		aux[this->nrPiese - 1] = pret;
+		if (this->pret != nullptr) {
+			delete[]this->pret;
+		}
+		this->pret = aux;
+		return *this;
+	}
+
+	Motocicleta operator+(float pret) const {
+		Motocicleta temp = *this;
+		float* aux = new float[temp.nrPiese + 1];
+		for (int i = 0; i < temp.nrPiese; i++) {
+			aux[i] = this->pret[i];
+		}
+		aux[temp.nrPiese] = pret;
+		if (temp.pret != nullptr) {
+			delete[]temp.pret;
+		}
+		temp.pret = aux;
+		temp.nrPiese++;
+		return temp;
+	}
+	
+
+
+
+	int getNrPiese() {
+		return this->nrPiese;
+	}
+
+
+
+	void setNrPiese(int nrPiese) {
+		if (nrPiese > 0) {
+			this->nrPiese = nrPiese;
+		}
+		else {
+			throw new exception();
+		}
+	}
+
+	
+	float* getPreturi() const {
+		//return this->pret;
+		float* vectorPreturi = new float[this->nrPiese];
+		for (int i = 0; i < this->nrPiese; i++) {
+			vectorPreturi[i] = this->pret[i];
+		}
+		return vectorPreturi;
+	}
+
+	void setPreturi(float* noulVectorPreturi, int nouaDimensiune) {
+		setNrPiese(nouaDimensiune);
+		if (this->pret != nullptr) {
+			delete[] this->pret;
+		}
+		this->pret = new float[nouaDimensiune];
+		for (int i = 0; i < nouaDimensiune; i++) {
+			this->pret[i] = noulVectorPreturi[i];
+		}
+		delete[] noulVectorPreturi;
+	}
+
+	const int getID()const {
+		return this->ID;
+	}
+	friend Motocicleta operator+(float pret, Motocicleta m);
+
+	~Motocicleta() {
+	
+		if (this->pret != nullptr)
+			delete[] this->pret;
+	}
+
+	void afisareMotocicleta() {
+		cout << this->ID << ". Motocicleta cu marca " << this->marca;
+		if (this->nrPiese > 0) {
+			cout << " are " << this->nrPiese << " componente cu urmatoarele pret:";
+			if (this->pret != nullptr) {
+				for (int i = 0; i < this->nrPiese; i++) {
+					cout << " " << this->pret[i];
+				}
+			}
+		}
+		cout << endl;
+	}
+
+	static int getNrMotociclete() {
+		return nrMotociclete;
+	}
+	static void setNrMotociclete(int _nrMotociclete) {
+		nrMotociclete = _nrMotociclete;
+	}
+
+};
+int Motocicleta::nrMotociclete = 0;
+
+
+Motocicleta operator+(float pret, Motocicleta m) {
+	Motocicleta temp = m + pret;
+	return temp;
+}
+
+
+int main() {
+
+	Motocicleta motocicleta1("Kawasaki", 3, new float[3] {1, 2, 3});
+
+	Motocicleta motocicleta2 = motocicleta1;
+	Motocicleta motocicleta3("Suzuki", 3, new float[3] {661, 635, 62});
+	
+
+	motocicleta3 += 1000;
+	motocicleta2 = motocicleta3 + 2000;
+
+	motocicleta2 = 3000 + motocicleta3;
+
+	motocicleta2.afisareMotocicleta();
+}
+
---
+++ play.cpp
@@ -0,0 +1,147 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+class Consola {
+private:
+    char* model;
+    int storage; // spațiu de stocare în GB
+    float price;
+
+public:
+    // Constructorul implicit
+    Consola() : model(nullptr), storage(0), price(0.0f) {
+        this->model = new char[10];
+        memcpy(this->model, "NU EXISTA\0", 10);
+        //cout << "Constructor implicit apelat\n";
+    }
+
+    // Constructor cu parametri
+    Consola(const char* model, int storage, float price) : storage(storage), price(price) {
+        if (model != nullptr && strlen(model) > 0 && strlen(model) < 15) {
+            this->model = new char[strlen(model) + 1];
+            strcpy(this->model, model);
+        }
+        else {
+            this->model = nullptr;  // Setăm modelul la nullptr dacă nu respectă condițiile
+        }
+
+        if (storage < 0) {
+            this->storage = 0;
+        }
+
+        if (price < 0.0f) {
+            this->price = 0.0f;
+        }
+
+        cout << "Constructor cu parametri apelat\n";
+    }
+
+    // Constructor de copiere
+    Consola(const Consola& other) {
+        if (other.model != nullptr) {
+            model = new char[strlen(other.model) + 1];
+            strcpy(model, other.model);
+        }
+        else {
+            model = nullptr;
+        }
+        storage = other.storage;
+        price = other.price;
+        cout << "Constructor de copiere apelat\n";
+    }
+
+    // Destructor
+    ~Consola() {
+        delete[] this->model;
+        this->model = nullptr;
+        cout << "Destructor apelat pentru consola " << (model ? model : "Necunoscut") << "\n";
+    }
+
+    // Getter pentru model
+    const char* getModel() {
+        return this->model;
+    }
+
+    // Setter pentru model
+    void setModel(const char* model) {
+
+        if (model != nullptr && strlen(model) > 0 && strlen(model) < 15) {
+            this->model = new char[strlen(model) + 1];
+            strcpy(this->model, model);
+        }
+        else {
+            this->model = nullptr;  // Setăm modelul la nullptr dacă nu respectă condițiile
+        }
+
+    }
+
+    // Getter pentru storage
+    int getStorage() {
+        return storage;
+    }
+
+    // Setter pentru storage
+    void setStorage(int storage) {
+        this->storage = storage >= 0 ? storage : 0;
+    }
+
+    // Getter pentru price
+    float getPrice() const {
+        return price;
+    }
+
+    // Setter pentru price
+    void setPrice(float price) {
+        this->price = price >= 0 ? price : 0.0f;
+    }
+
+    // Suprascrierea operatorului de atribuire (=)
+    Consola& operator=(const Consola& other) {
+        if (this != &other) {
+            delete[] model;
+            if (other.model != nullptr) {
+                model = new char[strlen(other.model) + 1];
+                strcpy(model, other.model);
+            }
+            else {
+                model = nullptr;
+            }
+            storage = other.storage;
+            price = other.price;
+        }
+        cout << "Operator de atribuire apelat\n";
+        return *this;
+    }
+
+    // Afișare informații despre consolă
+    void afisare() const {
+        cout << "Model: " << (model ? model : "Necunoscut") << ", Storage: " << storage << " GB, Price: $" << price << endl;
+    }
+};
+
+int main() {
+    // Crearea unui obiect Consola folosind constructorul cu parametri
+    Consola consola1("PlayBox", 500, 299.99);
+    consola1.afisare();
+
+    // Folosirea constructorului de copiere
+    Consola consola2 = consola1;
+    consola2.afisare();
+
+    // Folosirea operatorului de atribuire
+    Consola consola3;
+    consola3 = consola1;
+    consola3.afisare();
+
+    // Folosirea setterilor pentru a modifica valorile
+    consola3.setModel("GameStation");
+    consola3.setStorage(1000);
+    consola3.setPrice(499.99);
+    cout << "Detalii dupa modificare:\n";
+    consola3.afisare();
+
+    return 0;
+}
